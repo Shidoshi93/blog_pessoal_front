8507774d6fe6e0c0cf83f87367f6b4ed693244fd
Daniel Ribeiro <dann.ribeiroo@gmail.com>
2026-01-14 16:51:29 -0300

    feat: creates the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Home from './pages/home/Home.jsx'
+import NavBar from './components/navbar/NavBar.jsx'
 
 function App() {
 
   return (
     <>
+      <NavBar />
       <Home />
     </>
   )
